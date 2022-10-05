f3d435269c6a2d7ca792f2aeb53de83cf963d08b
Fauve6 <fauvewevers@gmail.com>
2022-10-05 15:58:49 +0200

    Added method for sensitivity at 95 specificity

--- MetricsPlots.py
+++ MetricsPlots.py
@@ -11,17 +11,18 @@ class MetricsPlots:
         self.predictions = predictions
         self.labels = labels
 
-    def get_auc_roc(self):
+    def get_partial_auc_roc(self):
         """
         Get the AUC-ROC score
 
         Return:
-             aucroc (float):
+             p_aucroc (float):
                 Returns the AUC-ROC score
         """
-        aucroc = metrics.roc_auc_score(self.labels, self.predictions)
+        min_spec = 0.9
+        p_aucroc = metrics.roc_auc_score(self.labels, self.predictions, max_fpr=(1 - min_spec))
 
-        return aucroc
+        return p_aucroc
 
     def get_metrics(self):
         """
@@ -36,8 +37,26 @@ class MetricsPlots:
 
         return metric
 
-    def get_sensitivity_95_specificity(self):
-        pass
+    def screening_sens_at_spec(self, at_spec, eps=sys.float_info.epsilon):
+        y_true = self.labels
+        y_pred = self.predictions
+
+        fpr, tpr, threshes = metrics.roc_curve(y_true, y_pred, drop_intermediate=False)
+        spec = 1 - fpr
+
+        operating_points_with_good_spec = spec >= (at_spec - eps)
+        max_tpr = tpr[operating_points_with_good_spec][-1]
+
+        operating_point = np.argwhere(operating_points_with_good_spec).squeeze()[-1]
+        operating_tpr = tpr[operating_point]
+
+        assert max_tpr == operating_tpr or (
+                np.isnan(max_tpr) and np.isnan(operating_tpr)), f'{max_tpr} != {operating_tpr}'
+        assert max_tpr == max(tpr[operating_points_with_good_spec]) or (
+                np.isnan(max_tpr) and max(tpr[operating_points_with_good_spec])), \
+            f'{max_tpr} == {max(tpr[operating_points_with_good_spec])}'
+
+        return max_tpr
 
     def plot_auc_roc(self):
         """
