6d572eed2f8a043d2d3d4cd8e4fe12f9ce2443da
valbuc <90454420+valbuc@users.noreply.github.com>
2022-10-07 15:03:32 +0200

    equalization ignoring black

---
+++ src/transformations.py
@@ -0,0 +1,112 @@
+import numpy as np
+from PIL import Image
+from torchvision.transforms.functional import equalize
+import cv2
+import torch
+
+class EqualizeIgnoreBlack(object):
+    """Normalize a tensor image with mean and standard deviation.
+    Given mean: ``(M1,...,Mn)`` and std: ``(S1,..,Sn)`` for ``n`` channels, this transform
+    will normalize each channel of the input ``torch.*Tensor`` i.e.
+    ``input[channel] = (input[channel] - mean[channel]) / std[channel]``
+
+    Args:
+        mean (sequence): Sequence of means for each channel.
+        std (sequence): Sequence of standard deviations for each channel.
+    """
+
+    def __init__(self, threshold, kernel_size):
+        self.threshold = threshold
+        self.kernel_size = kernel_size
+
+    def __call__(self, PilImage):
+        """
+        Args:
+            tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
+
+        Returns:
+            Tensor: Normalized Tensor image.
+        """
+        img = np.array(PilImage)
+
+        # get mask
+        grey = np.max(img, axis = 2)
+        mask = grey > self.threshold
+        mask = mask.astype(float)
+
+        # remove noise from mask
+        mask_closed = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, np.ones((self.kernel_size, self.kernel_size)))
+        mask_inverted = mask_closed == 0
+
+        # make mask 3d
+        mask_inverted = mask_inverted.reshape((mask_inverted.shape[0], mask_inverted.shape[1], 1))
+        mask_inv_3d = np.concatenate((mask_inverted, mask_inverted, mask_inverted), axis=2)
+
+        # replace black with uniform noise
+        count = np.count_nonzero(mask_inv_3d)
+        uniform_noise = np.random.randint(0, 255, count)
+        img[mask_inv_3d] = uniform_noise
+
+        # equalize
+        equal = equalize(Image.fromarray(img))
+
+        # substitute noise
+        equal = np.array(equal)
+        equal[mask_inv_3d] = 123
+        return Image.fromarray(equal)
+
+
+class Equalize(object):
+    """Normalize a tensor image with mean and standard deviation.
+    Given mean: ``(M1,...,Mn)`` and std: ``(S1,..,Sn)`` for ``n`` channels, this transform
+    will normalize each channel of the input ``torch.*Tensor`` i.e.
+    ``input[channel] = (input[channel] - mean[channel]) / std[channel]``
+
+    Args:
+        mean (sequence): Sequence of means for each channel.
+        std (sequence): Sequence of standard deviations for each channel.
+    """
+
+    def __init__(self):
+        pass
+
+    def __call__(self, array):
+        """
+        Args:
+            tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
+
+        Returns:
+            Tensor: Normalized Tensor image.
+        """
+        return equalize(array)
+
+
+class Normalize(object):
+    """Normalize a tensor image with mean and standard deviation.
+    Given mean: ``(M1,...,Mn)`` and std: ``(S1,..,Sn)`` for ``n`` channels, this transform
+    will normalize each channel of the input ``torch.*Tensor`` i.e.
+    ``input[channel] = (input[channel] - mean[channel]) / std[channel]``
+
+    Args:
+        mean (sequence): Sequence of means for each channel.
+        std (sequence): Sequence of standard deviations for each channel.
+    """
+
+    def __init__(self, mean, std):
+        self.mean = torch.tensor(mean)
+        self.std = torch.tensor(std)
+
+    def __call__(self, tensor):
+        """
+        Args:
+            tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
+
+        Returns:
+            Tensor: Normalized Tensor image.
+        """
+        x = (tensor - self.mean[:, None, None]) / self.std[:, None, None]
+        return x
+
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
